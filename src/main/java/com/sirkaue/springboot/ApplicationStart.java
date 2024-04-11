9ece32fb0d6eff77f3309eb6eb5b4f1dc14319bf
sirkaue <kauekdslopes@gmail.com>
2024-04-11 18:31:31 -0300

    Adicionando Injenção de Dependencia, util

--- src/main/java/com/sirkaue/springboot/ApplicationStart.java
+++ src/main/java/com/sirkaue/springboot/ApplicationStart.java
@@ -1,11 +1,9 @@
-package com.sirkaue.springboot.start;
+package com.sirkaue.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = "com.sirkaue.springboot")
 public class ApplicationStart {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStart.class, args);
--- src/main/java/com/sirkaue/springboot/controller/AnimeController.java
+++ src/main/java/com/sirkaue/springboot/controller/AnimeController.java
@@ -1,17 +1,31 @@
 package com.sirkaue.springboot.controller;
 
 import com.sirkaue.springboot.domain.Anime;
+import com.sirkaue.springboot.util.DateUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
 @RequestMapping("anime")
 public class AnimeController {
+    private static final Logger log = LogManager.getLogger(AnimeController.class);
+
+    private DateUtil dateUtil;
+
+    // Injeção de Dependência via método construtor
+    public AnimeController(DateUtil dateUtil) {
+        this.dateUtil = dateUtil;
+    }
+
     @GetMapping(path = "list")
     public List<Anime> list() {
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return List.of(new Anime("DBZ"), new Anime("Beserker"));
     }
 }
