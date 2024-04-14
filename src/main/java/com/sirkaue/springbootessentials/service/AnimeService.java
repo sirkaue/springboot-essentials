f3fe38d0d4d74d5a330647216c7fb40c1f86f113
sirkaue <kauekdslopes@gmail.com>
2024-04-14 12:27:30 -0300

    Adicionando Método POST

--- src/main/java/com/sirkaue/springbootessentials/service/AnimeService.java
+++ src/main/java/com/sirkaue/springbootessentials/service/AnimeService.java
@@ -5,12 +5,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Service
 public class AnimeService {
     //private final AnimeRepository animeRepository;
-    private List<Anime> animes = List.of(new Anime(1L, "DBZ"), new Anime(2L, "Beserker"));
+    private static List<Anime> animes;
+
+    static {
+        animes = new ArrayList<>(List.of(new Anime(1L, "DBZ"), new Anime(2L, "Beserker")));
+    }
 
     public List<Anime> listAll() {
         return animes;
@@ -21,5 +27,15 @@ public class AnimeService {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
+    }
+
+    public Anime save(Anime anime) {
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        animes.add(anime);
+        return anime;
+    }
+
+    public void delete(Long id) {
+        animes.remove(findById(id));
     }
 }
