c24cc914cd460b421e519cb52820c65cd25d2d5e
sirkaue <kauekdslopes@gmail.com>
2024-04-17 17:57:29 -0300

    Spring Data JPA pt 01

--- src/main/java/com/sirkaue/springbootessentials/domain/Anime.java
+++ src/main/java/com/sirkaue/springbootessentials/domain/Anime.java
@@ -1,8 +1,16 @@
 package com.sirkaue.springbootessentials.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 import java.util.Objects;
 
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
